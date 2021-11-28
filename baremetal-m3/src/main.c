f1de9a2b75a3a83ef04c039a90767609a248b8df
Jinping Wu <jinping.wu@verisilicon.com>
2021-11-28 11:25:34 +0800

    bm-m3 : add sys ctrl

--- baremetal-m3/src/main.c
+++ baremetal-m3/src/main.c
@@ -5,6 +5,7 @@
  */
 
 #include <console.h>
+#include <sys_ctrl.h>
 
 int global_variable_data = 0x11223344;
 int global_variable_bss1;
@@ -12,17 +13,26 @@ int global_variable_bss1;
 void printf_test(void)
 {
 	unsigned int test = 0x12345678;
+	unsigned char test_u8 = 0x88;
 
 	global_variable_bss1 = 0x3333;
-	console_init();
 	bm_printf("Enter _main!\n");
-	bm_printf_value("This is test value:", test);
-	bm_printf_value("The golbal variable is :", global_variable_data);
-	bm_printf_value("The golbal variable is :", global_variable_bss1);
+	bm_printf_value_u32("This is test value:", test);
+	bm_printf_value_u32("The golbal variable is :", global_variable_data);
+	bm_printf_value_u32("The golbal variable is :", global_variable_bss1);
+	bm_printf_value_u8("unsigned char print test :", test_u8);
+	bm_printf_value_u8("unsigned char print test :", test_u8);
+}
+
+void platform_init(void)
+{
+	console_init();
+	sys_ctrl_init();
 }
 
 int main(void)
 {
+	platform_init();
 	printf_test();
 	return 0;
 }
