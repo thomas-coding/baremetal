33683e3dad5350e98033817588f132f43a8f93de
Jinping Wu <jinping.wu@verisilicon.com>
2021-12-29 16:51:03 +0800

    bm-m3 : uart add receive test and rx interrupt test

--- baremetal-m3/src/board/apbuart/apbuart.c
+++ baremetal-m3/src/board/apbuart/apbuart.c
@@ -6,26 +6,13 @@
 
 #include <apbuart.h>
 
-typedef struct UART_t {
-	volatile unsigned int DATA;
-	volatile unsigned int STATE;
-	volatile unsigned int CTRL;
-	volatile unsigned int INTSTATUS;
-	volatile unsigned int BAUDDIV;
-} UART_t;
-
-#define UART0_ADDR ((UART_t *)(0x40004000))
-
-#define UART_CTRL_TX_EN (1 << 0)
-#define UART_CTRL_RX_EN (1 << 1)
-
 /**
  * Initializes the UART
  */
 void uart_init(void)
 {
 	UART0_ADDR->BAUDDIV = 16;
-	UART0_ADDR->CTRL = UART_CTRL_TX_EN;
+	UART0_ADDR->CTRL = UART_CTRL_TX_EN | UART_CTRL_RX_EN | UART_CTRL_RX_INTR_EN;
 }
 
 /**
@@ -34,5 +21,24 @@ void uart_init(void)
 void uart_putc(char c)
 {
 	UART0_ADDR->DATA =  c;
+}
+
+// Get a character
+unsigned char uart_getc(void)
+{
+    unsigned char my_ch;
+
+    while ((UART0_ADDR->STATE & 2) == 0) // Wait if Receive
+    {
+
+    }
+
+    my_ch = UART0_ADDR->DATA;
+
+    // Convert CR to LF
+    if (my_ch == '\r')
+        my_ch = '\n';
+
+    return (my_ch);
 }
 
--- baremetal-m3/src/common/console.c
+++ baremetal-m3/src/common/console.c
@@ -5,6 +5,8 @@
  */
 
 #include <apbuart.h>
+#include <nvic.h>
+#include <sys_ctrl.h>
 
 #define U32_MAX_DIGIT_BITS 12  /* 8 char for number, 2 for '0x' and 1 for '\n', last 0 for end */
 #define U8_MAX_DIGIT_BITS 6  /* 2 char for number, 2 for '0x' and 1 for '\n', last 0 for end */
@@ -77,7 +79,56 @@ void bm_printf_value_u8(char *s, unsigned char value)
 	bm_printf_hex_u8(value);
 }
 
+void uart_rx_isr(void)
+{
+	/* Clear interrupt */
+	UART0_ADDR->INTSTATUS = 0x3;
+	bm_printf("Enter uart_rx_isr!\n");
+}
+
+#define UART_RX_IRQ 0
+#define NVIC_USER_IRQ_OFFSET          16
+void uart_receive_interrupt_test()
+{
+	bm_printf("Enter uart_receive_interrupt_test\n");
+
+	/* Before test, Clear interrupt */
+	UART0_ADDR->INTSTATUS = 0x3;
+	NVIC->ICPR[(((uint32_t)UART_RX_IRQ) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)UART_RX_IRQ) & 0x1FUL));
+
+	/* Enable this interrupt */
+	NVIC->ISER[(((uint32_t)UART_RX_IRQ) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)UART_RX_IRQ) & 0x1FUL));
+
+	/* Get the vectors table offset , now is 0x00000000 */
+	uint32_t *vectors = (uint32_t *)SCB->VTOR;
+
+	vectors[(uint32_t)(UART_RX_IRQ) + NVIC_USER_IRQ_OFFSET] = (uint32_t)uart_rx_isr;
+
+	bm_printf("Enter uart_receive_interrupt_test, please enter char...\n");
+	unsigned char my_ch = uart_getc();
+
+	uart_putc(my_ch);
+	bm_printf("\n");
+
+	/* Disable this interrupt and clear vector */
+	NVIC->ICER[(((uint32_t)UART_RX_IRQ) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)UART_RX_IRQ) & 0x1FUL));
+	vectors[(uint32_t)(UART_RX_IRQ) + NVIC_USER_IRQ_OFFSET] = 0;
+
+}
+
+void uart_receive_test()
+{
+	bm_printf("Enter uart_test, please enter char...\n");
+	unsigned char my_ch = uart_getc();
+	bm_printf("uart_test after get char:\n");
+	uart_putc(my_ch);
+	bm_printf("\n");
+}
+
 void console_init(void)
 {
 	uart_init();
+	/* Receive test is disabled default, can enable it for free */
+	//uart_receive_test();
+	//uart_receive_interrupt_test();
 }
