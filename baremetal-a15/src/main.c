4de99b5d9cc049897c3a1db2afa63fd72d9c4ec0
Jinping Wu <jinping.wu@verisilicon.com>
2022-01-10 17:14:02 +0800

    bm-a15 : add ram, add stack, then can run c function

---
+++ baremetal-a15/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021-2031, Jinping Wu. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+void platform_init(void)
+{
+
+}
+
+int main(void)
+{
+	platform_init();
+	return 0;
+}
