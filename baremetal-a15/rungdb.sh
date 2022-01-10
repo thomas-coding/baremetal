dafebfdc3991f3dee7245f476cb6241885ec2b83
Jinping Wu <jinping.wu@verisilicon.com>
2022-01-10 17:11:25 +0800

    bm-a15 : add debug, update toolchain path

--- baremetal-a15/rungdb.sh
+++ baremetal-a15/rungdb.sh
@@ -5,6 +5,7 @@ shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
 
 
 export PATH="/root/workspace/.toolchains/gcc-arm-10.3-2021.07-x86_64-arm-none-eabi/bin/:$PATH"
+export PATH="/home/cn1396/.toolchain/gcc-arm-10.3-2021.07-x86_64-arm-none-eabi/bin/:$PATH"
 
 # gdb
 arm-none-eabi-gdb \
