04904b88c7b3107104827f7121f2da143a883e56
Jinping Wu <jinping.wu@verisilicon.com>
2022-01-11 14:08:22 +0800

    bm-a15 : add bss section and zero it for golbal variable, not inited

--- baremetal-a15/src/main.c
+++ baremetal-a15/src/main.c
@@ -13,12 +13,16 @@ void platform_init(void)
 
 unsigned int golbal_variable_initialized1 = 0x88;
 unsigned int golbal_variable_initialized2 = 0x77;
+unsigned int golbal_variable_not_init;
 void data_and_bss_test()
 {
 	golbal_variable_initialized1++;
 	golbal_variable_initialized2--;
+	bm_printf_value_u32("golbal_variable_not_init before:",golbal_variable_not_init);
+	golbal_variable_not_init = 0x99;
 	bm_printf_value_u32("golbal_variable_initialized1:",golbal_variable_initialized1);
 	bm_printf_value_u32("golbal_variable_initialized2:",golbal_variable_initialized2);
+	bm_printf_value_u32("golbal_variable_not_init after:",golbal_variable_not_init);
 }
 
 int main(void)
