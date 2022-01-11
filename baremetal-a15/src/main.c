58fa32235afad44faeebfec945af1fc42b38654e
Jinping Wu <jinping.wu@verisilicon.com>
2022-01-11 13:32:37 +0800

    bm-a15 : add data section and copy for golbal variable

--- baremetal-a15/src/main.c
+++ baremetal-a15/src/main.c
@@ -11,9 +11,20 @@ void platform_init(void)
 	console_init();
 }
 
+unsigned int golbal_variable_initialized1 = 0x88;
+unsigned int golbal_variable_initialized2 = 0x77;
+void data_and_bss_test()
+{
+	golbal_variable_initialized1++;
+	golbal_variable_initialized2--;
+	bm_printf_value_u32("golbal_variable_initialized1:",golbal_variable_initialized1);
+	bm_printf_value_u32("golbal_variable_initialized2:",golbal_variable_initialized2);
+}
+
 int main(void)
 {
 	platform_init();
+	data_and_bss_test();
 	bm_printf("enter main\n");
 	return 0;
 }
