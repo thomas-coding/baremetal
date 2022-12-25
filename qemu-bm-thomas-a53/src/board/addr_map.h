5a693a7a77c1892deda2c5f7d09296cdad8439bd
Jinping Wu <jinping.wu@verisilicon.com>
2022-12-25 13:13:52 +0800

    add uart

---
+++ qemu-bm-thomas-a53/src/board/addr_map.h
@@ -0,0 +1,13 @@
+/*
+ * Header for address map
+ *
+ * Copyright (C) 2022 VeriSilicon Holdings Co., Ltd.
+ *
+ */
+
+#ifndef __ADDR_MAP_H__
+#define __ADDR_MAP_H__
+
+#define UART0_BASE              0x40000000
+
+#endif /* __ADDR_MAP_H__ */
--- qemu-bm-thomas-a53/src/main.c
+++ qemu-bm-thomas-a53/src/main.c
@@ -4,10 +4,18 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include "devices.h"
+#include "common.h"
 
 int main(void)
 {
 	int count=0;
 	count++;
+
+	devices_init();
+
+	vs_printf("enter main\n");
+
+	while(1) {};
 	return 0;
 }
