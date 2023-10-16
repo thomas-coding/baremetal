71fb79d2781a8c41cff876930695c3851957d3aa
Jinping Wu <jinping.wu@verisilicon.com>
2023-10-16 11:26:00 +0800

    add riscv64

---
+++ baremetal-riscv64/runmonitor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# shell folder
+#shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
+
+telnet localhost 65530
+ 
---
+++ baremetal-riscv64/src/ips/16550/16550.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021-2031, Jinping Wu. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef _16550_H
+#define _16550_H
+
+// the UART control registers are memory-mapped
+// at address UART0. this macro returns the
+// address of one of the registers.
+#define Reg(reg) ((volatile unsigned char *)(UART0 + reg))
+
+// the UART control registers.
+// some have different meanings for
+// read vs write.
+// see http://byterunner.com/16550.html
+#define RHR 0                 // receive holding register (for input bytes)
+#define THR 0                 // transmit holding register (for output bytes)
+#define IER 1                 // interrupt enable register
+#define IER_RX_ENABLE (1<<0)
+#define IER_TX_ENABLE (1<<1)
+#define FCR 2                 // FIFO control register
+#define FCR_FIFO_ENABLE (1<<0)
+#define FCR_FIFO_CLEAR (3<<1) // clear the content of the two FIFOs
+#define ISR 2                 // interrupt status register
+#define LCR 3                 // line control register
+#define LCR_EIGHT_BITS (3<<0)
+#define LCR_BAUD_LATCH (1<<7) // special mode to set baud rate
+#define LSR 5                 // line status register
+#define LSR_RX_READY (1<<0)   // input is waiting to be read from RHR
+#define LSR_TX_IDLE (1<<5)    // THR can accept another character to send
+
+#define UART_APB_CLOCK	24000000
+#define UART_BAND_RATE	115200
+
+#define UART0_ADDR 0x10000000L
+
+
+void uart_init(void);
+void uart_putc(char c);
+
+#endif
